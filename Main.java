7b03e541b4a1e3a0fa9c7dc603903f2ace1c4ca6
BoweS2027 <bowe.siegelson2027@gbwl.org>
2026-02-19 19:25:09 +0000

    dewsrfg

--- Main.java
+++ Main.java
@@ -1,6 +1,20 @@
 public class Main {
 
    public static void main(String []args) {
-      System.out.println("Hello World");
+      //4.7 wrapper classes
+      Integer fancyInt = new Integer(1);
+      Double fancyDoub = new Double(1.1);
+      // calling the constructor is no longer necessary
+      Integer fancierInt = 13;
+      System.out.println(fancierInt);
+      int regularInt = fancierInt;
+      System.out.println(Integer.MIN_VALUE);
+      System.out.println(Integer.MAX_VALUE);
+      String stringyNums = "13.0";
+      double parsedD = Double.parseDouble(stringyNums);
+      System.out.println(parsedD);
+      String stringyNums2 = "13";
+      int parsedI = Integer.parseInt(stringyNums2);
+      System.out.println(parsedI);
    }
 }
